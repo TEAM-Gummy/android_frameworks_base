b7c2bb5560990c3035bd46d8033c84e3999e5024
masiullah <masiullah@gmail.com>
2012-07-14 19:05:57 +0530

    Stylus gestures features (1/2)
    Forward-port of https://github.com/CyanogenMod/android_frameworks_base/commit/9f3ee9f7c0877fcfbe90a7c176481f949194fb75
    to JB
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	core/res/res/values/config.xml
    	core/res/res/values/strings.xml
    	core/res/res/values/symbols.xml
    	policy/src/com/android/internal/policy/impl/PhoneWindow.java
    
    Change-Id: I9c8a03dce226d2463659846ff3a12435efdf5c1d

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3314,6 +3314,56 @@ public final class Settings {
         public static final String DCLICK_TOGGLE_REVERT = "dclick_toggle_revert";
 
         /**
+         * Enable Stylus Gestures
+         *
+         * @hide
+         */
+        public static final String ENABLE_STYLUS_GESTURES = "enable_stylus_gestures";
+
+        /**
+         * Left Swipe Action
+         *
+         * @hide
+         */
+        public static final String GESTURES_LEFT_SWIPE = "gestures_left_swipe";
+
+        /**
+         * Right Swipe Action
+         *
+         * @hide
+         */
+        public static final String GESTURES_RIGHT_SWIPE = "gestures_right_swipe";
+
+        /**
+         * Up Swipe Action
+         *
+         * @hide
+         */
+        public static final String GESTURES_UP_SWIPE = "gestures_up_swipe";
+
+        /**
+         * down Swipe Action
+         *
+         * @hide
+         */
+        public static final String GESTURES_DOWN_SWIPE = "gestures_down_swipe";
+
+        /**
+         * Long press Action
+         *
+         * @hide
+         */
+        public static final String GESTURES_LONG_PRESS = "gestures_long_press";
+
+        /**
+         * double tap Action
+         *
+         * @hide
+         */
+        public static final String GESTURES_DOUBLE_TAP = "gestures_double_tap";
+
+        /**
+         * Whether power menu airplane toggle is enabled
          * @hide
          */
         public static final String MATCH_ACTION_ICON = "match_action_icon";
