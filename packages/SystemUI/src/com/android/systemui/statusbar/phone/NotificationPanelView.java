8d2f44fb875a46ce30f4bd8f65bc526a3b972620
Roman Birg <romanbirg@gmail.com>
2012-11-25 00:28:21 -0600

    SystemUI: open settings panel if there are no notifications
    If there are no notifications that belong to the current user, then automatically open the quick settings menu (like the two-finger open). It only ignores the ADB icon for now (which is pretty useless) but could be extended to ignore the usb mounting one or maybe another?
    
    TODO: add Settings.System toggle to disable the option
    
    Change-Id: I3b161ae4fc985bcc26b22f4be9ab045036508278
    Signed-off-by: Roman Birg <romanbirg@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
@@ -111,9 +111,14 @@ public class NotificationPanelView extends PanelView {
             }
         }
         if (PhoneStatusBar.SETTINGS_DRAG_SHORTCUT && mStatusBar.mHasFlipSettings) {
+            boolean shouldFlip = false;
+
             switch (event.getActionMasked()) {
                 case MotionEvent.ACTION_DOWN:
                     mOkToFlip = getExpandedHeight() == 0;
+                    if(mStatusBar.skipToSettingsPanel()) {
+                        shouldFlip = true;
+                    }
                     break;
                 case MotionEvent.ACTION_POINTER_DOWN:
                     if (mOkToFlip) {
@@ -125,15 +130,18 @@ public class NotificationPanelView extends PanelView {
                             if (y > maxy) maxy = y;
                         }
                         if (maxy - miny < mHandleBarHeight) {
-                            if (getMeasuredHeight() < mHandleBarHeight) {
-                                mStatusBar.switchToSettings();
-                            } else {
-                                mStatusBar.flipToSettings();
-                            }
-                            mOkToFlip = false;
+                            shouldFlip = true;
                         }
                     }
                     break;
+            }
+            if(mOkToFlip && shouldFlip) {
+                if (getMeasuredHeight() < mHandleBarHeight) {
+                    mStatusBar.switchToSettings();
+                } else {
+                    mStatusBar.flipToSettings();
+                }
+                mOkToFlip = false;
             }
         }
         return mHandleView.dispatchTouchEvent(event);
