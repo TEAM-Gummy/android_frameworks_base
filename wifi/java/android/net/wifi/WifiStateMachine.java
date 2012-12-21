df489e66697782c2e21b055f9858dfadb093e2e5
Steve Kondik <shade@chemlab.org>
2012-12-20 20:40:52 -0800

    wifi: Set the mode at native layer
     * Set normal/AP mode so that the HAL can specify correct module
       arguments when loading the driver.
    
    Conflicts:
    	wifi/java/android/net/wifi/WifiNative.java
    	wifi/java/android/net/wifi/WifiStateMachine.java
    
    Change-Id: I6eee2cc53b5f1e70306e252e52c909833f220515

--- wifi/java/android/net/wifi/WifiStateMachine.java
+++ wifi/java/android/net/wifi/WifiStateMachine.java
@@ -760,6 +760,7 @@ public class WifiStateMachine extends StateMachine {
      */
     public void setSupplicantRunning(boolean enable) {
         if (enable) {
+            WifiNative.setMode(0);
             sendMessage(CMD_START_SUPPLICANT);
         } else {
             sendMessage(CMD_STOP_SUPPLICANT);
@@ -772,6 +773,7 @@ public class WifiStateMachine extends StateMachine {
     public void setHostApRunning(WifiConfiguration wifiConfig, boolean enable) {
         if (enable) {
             sendMessage(CMD_START_AP, wifiConfig);
+            WifiNative.setMode(1);
         } else {
             sendMessage(CMD_STOP_AP);
         }
