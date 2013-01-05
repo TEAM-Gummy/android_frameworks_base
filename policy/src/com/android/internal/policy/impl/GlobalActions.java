116102be11a22e5e6d483d1d2832ba42289ac80e
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2013-01-05 21:22:20 +0000

    GlobalActions: Fix possible race condition when showing dialog.
    Depending on the presence of a previous dialog, the newly generated one
    gets either immediately displayed (no old dialog) or shortly delayed
    (after dismissal of the existing one).
    If a new show event happens (by rapid fire of the power key, for instance,
    using a keyboard with fast-repeating events) during that delay, a new
    dialog will be immediately displayed, and when the delayed show triggers
    it'll conflict with it (and bring everything down).
    
    This moves the creation of the new Dialog object to _before_ showing
    it (delayed or not), instead of the show method itself, to ensure the
    object isn't clobbered by competing/sequential calls to handleShow()
    
    Change-Id: Ie4d9a8507ebaf59da31869c38b98c3c98a74468e

--- policy/src/com/android/internal/policy/impl/GlobalActions.java
+++ policy/src/com/android/internal/policy/impl/GlobalActions.java
@@ -168,9 +168,11 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
         if (mDialog != null && mUiContext == null) {
             mDialog.dismiss();
             mDialog = null;
+            mDialog = createDialog();
             // Show delayed, so that the dismiss of the previous dialog completes
             mHandler.sendEmptyMessage(MESSAGE_SHOW);
         } else {
+            mDialog = createDialog();
             handleShow();
         }
     }
@@ -198,7 +200,6 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
         }
 
         awakenIfNecessary();
-        mDialog = createDialog();
         prepareDialog();
 
         final IStatusBarService barService = IStatusBarService.Stub.asInterface(
