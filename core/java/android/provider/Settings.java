c2e62f330a001a199040faf5b6382e9dc678879c
Lars Greiss <kufikugel@googlemail.com>
2013-05-24 10:16:26 +0200

    Frameworks: TRDS 2.0 (1/3)
    After testing with huge success we discovered here
    and there small problems (like NFC beam animation is broken,
    TRDS was disabled on cardock etc) which mainly was caused
    due that we used the already from AOSP introduced uiModes
    qualifier which could cause in special conditions this
    conflicts. Beside that we want a bit more flexibility.
    
    Result: This commit introduces the inverted qualifier as
    complete own qualifier undependent of the already AOSP
    used ones.
    
    Beside that it fixes all known issues......features:
    
    - it is now completly working over the AOSP Configuration classes
      Means every app get notice of a change trough onConfiguratioChange
      and restarts it's activity if change was detected.
    - app can disable that like some other configuration values
      over AndroidMainfest.xml
    - beside "inverted" folder another "notinverted" folder is introduced
      for another themeing (eg white)
    - qualifier order is direct before the official uiMode qualifiers
      (see AOSP docs)
    - All usual Configuration actions can be used with the inverted
      qualifiers....like BroadcastChange, direct read out from every
      app etc. etc. Note: Only system can change the uiInvertedMode
    
    So as you see a real own qualifier now
    
    NOTE: Apktool as well aapt need to be changed to fit the "new
    qualifier"....which means that we need to recompile our gapps
    that they react as well on this new behaviour. Think we will get them ready
    next 2 or 3 days.
    
    Patchset:
    increase support from SDK_HONEYCOMB_MR2
    to FROYO to have better compatibillity
    to some gapps.
    
    Change-Id: I851740010198c400266d7ca9995eb08dbf7782bc
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/WidgetView.java
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
    
    Conflicts:
    	api/10.xml
    	api/11.xml
    	api/12.xml
    	api/8.xml
    	api/9.xml
    	core/java/android/provider/Settings.java
    	packages/SystemUI/src/com/android/systemui/recent/InterestingConfigChanges.java
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/WidgetView.java
    
    Change-Id: I851740010198c400266d7ca9995eb08dbf7782bc

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4482,13 +4482,6 @@ public final class Settings {
         public static final String UI_INVERTED_MODE = "ui_inverted_mode";
 
         /**
-         * force UI mode change for methods which are not observing
-         * UiModeManagerService
-         * @hide
-         */
-        public static final String UI_MODE_IS_TOGGLED = "ui_mode_is_toggled";
-
-        /**
          * Whether screensavers are enabled.
          * @hide
          */
@@ -4587,7 +4580,6 @@ public final class Settings {
             UI_NIGHT_MODE,
             DIALPAD_AUTOCOMPLETE
             UI_INVERTED_MODE,
-            UI_MODE_IS_TOGGLED,
         };
 
         /**
