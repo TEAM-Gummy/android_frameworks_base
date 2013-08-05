97bad73e800553d6387a72ad9d32f853ab839e52
cphelps76 <cphelps76@gmail.com>
2013-08-05 17:37:32 -0500

    Frameworks: breathing Mms and missed call notifications (1/4)
    Change-Id: I8db110777ea9a6a0dd359aa2e2bb46d16824544b

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3403,6 +3403,18 @@ public final class Settings {
          */
         public static final String QUIET_HOURS_DIM = "quiet_hours_dim";
 
+        /**  
+         * Give MMS Notifications a breathing effect
+         * @hide
+         */
+        public static final String MMS_BREATH = "mms_breath";
+
+        /**  
+         * Give MMS Notifications a breathing effect
+         * @hide
+         */
+        public static final String MISSED_CALL_BREATH = "missed_call_breath";
+
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
