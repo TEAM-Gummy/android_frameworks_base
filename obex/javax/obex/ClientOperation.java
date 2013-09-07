f49d712989f1bc47e6125c56a5159cf39a354251
Steve Kondik <shade@chemlab.org>
2013-09-07 09:11:15 -0700

    Revert "Ported: Bluetooth Message Access Profile (MAP) from CM9"
    This reverts commit 0e5fdcffdd1c3f1292a7cfa04660c966f70c883a.
    
    MAP: Support MAP Server role on Bluedroid with javax.OBEX lib.
    
    Handle No END OF BODY for MAP 1.0 Server Get request. Only the
    last packet need send the 0x49(End of Body). For intermediate
    packets, we need to send 0x48 (Body).
    
    Move definitions for MAP or FTP Service Specific UUID and
    Request type for CONNECTION_ACCESS_REQUEST intent to
    BluetoothExt APK.
    
    Change-Id: I04cfeb8d53b8f8c19160a2de1eb448a7ec1f2c41
    CRs-fixed: 504042

--- obex/javax/obex/ClientOperation.java
+++ obex/javax/obex/ClientOperation.java
@@ -1,5 +1,6 @@
 /*
- * Copyright (c) 2010-2011, Motorola, Inc.
+ * Copyright (c) 2013, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2008-2009, Motorola, Inc.
  *
  * All rights reserved.
  *
@@ -122,12 +123,6 @@ public final class ClientOperation implements Operation, BaseStream {
             mRequestHeader.mAuthResp = new byte[(header).mAuthResp.length];
             System.arraycopy((header).mAuthResp, 0, mRequestHeader.mAuthResp, 0,
                     (header).mAuthResp.length);
-
-        }
-        if ((header).mConnectionID != null) {
-            mRequestHeader.mConnectionID = new byte[4];
-            System.arraycopy((header).mConnectionID, 0, mRequestHeader.mConnectionID, 0,
-                    4);
 
         }
     }
@@ -760,7 +755,6 @@ public final class ClientOperation implements Operation, BaseStream {
             }
         }
     }
-
     public void noEndofBody() {
 
     }
