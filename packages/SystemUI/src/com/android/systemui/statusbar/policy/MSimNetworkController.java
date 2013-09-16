3b1ebe6f9ecad7122f882772a9e36f0682c19376
Suchand Ghosh <suchan@codeaurora.org>
2013-09-16 11:08:24 +0530

    SystemUI (MSIM): Fix Airplane mode toggle issue using Quick Access menu.
    onAirplaneModeChanged() in QuickSettingsModel class needs to be called from
    MSimNetworkController class (for MSIM) to update Airplane widget in Quick
    Access menu. Same code is missing in MSimNetworkController class.
    Add notifySignalsChangedCallbacks() at MSimNetworkController class.
    
    Change-Id: I214f679b377f26fb58b6165a52c5e12c860f5111
    CRs-Fixed: 543170

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/MSimNetworkController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/MSimNetworkController.java
@@ -1158,6 +1158,9 @@ public class MSimNetworkController extends NetworkController {
             for (MSimSignalCluster cluster : mSimSignalClusters) {
                 refreshSignalCluster(cluster, subscription);
             }
+            for (NetworkSignalChangedCallback cb : mSignalsChangedCallbacks) {
+                notifySignalsChangedCallbacks(cb);
+            }
         }
 
         if (mLastAirplaneMode != mAirplaneMode) {
