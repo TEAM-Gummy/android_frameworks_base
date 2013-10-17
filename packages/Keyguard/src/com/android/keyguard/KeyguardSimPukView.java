45bf9341887c075fe1b6083fa09cc2c2bf297a4c
Chaitanya Saggurthi <csaggurt@codeaurora.org>
2013-10-17 18:19:43 +0530

    Telephony(MSIM): MultiSim support for PIN/PUK & Operator Name.
    - Add CarrierText, SimPinView and SimPukView classes for MSIM.
      Add support to Display Multiple operator name.
      Add Support for Multiple PIN/PUK Lock screens.
      Overload few Keyguard methods to take sub info.
    
    Add keyguard simpin/simpuk RTL-ization
     -Add support for SIM PIN and SIM PUK keyboards to keep the
      keys in the same position for all languages.
    
    Add DETECTED state and PIN/PUK retry count.
     - Add App state DETECTED in the IccCardConstants.State enum.
       Modified logic for allowing PIN cancel based on number of
       cards configured/activated instead of number of cards present.
     - Add method in ITelephonyMSim.aidl to retrieve PIN1RetryCount.
       Display Attempts remaining in PIN/PUK lockscreen UI.
     - Clean up KeyguardUpdateMonitorCallback and MSimCarrierText by
       removing Initialize method from Callback.
    
    Change-Id: I500129a5dc09adcb2cd8eb664d30ae2e8bad2642
    (cherry picked from commit 13545ca99ffd932d9d04a94fb20f9669845b6f74)
    (cherry picked from commit 1d7a713e73d4bd1964f62f8ae38852074bc0a9ad)
    
    Conflicts:
    	packages/Keyguard/src/com/android/keyguard/KeyguardSimPinView.java
    	packages/Keyguard/src/com/android/keyguard/KeyguardSimPukView.java

--- packages/Keyguard/src/com/android/keyguard/KeyguardSimPukView.java
+++ packages/Keyguard/src/com/android/keyguard/KeyguardSimPukView.java
@@ -1,4 +1,6 @@
 /*
+ * Copyright (c) 2013, The Linux Foundation. All rights reserved.
+ * Not a Contribution.
  * Copyright (C) 2012 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -47,19 +49,19 @@ public class KeyguardSimPukView extends KeyguardAbsKeyInputView
     private static final boolean DEBUG = KeyguardViewMediator.DEBUG;
     public static final String TAG = "KeyguardSimPukView";
 
-    private ProgressDialog mSimUnlockProgressDialog = null;
+    protected ProgressDialog mSimUnlockProgressDialog = null;
     private CheckSimPuk mCheckSimPukThread;
-    private String mPukText;
-    private String mPinText;
-    private StateMachine mStateMachine = new StateMachine();
+    protected String mPukText;
+    protected String mPinText;
+    protected StateMachine mStateMachine = new StateMachine();
     private AlertDialog mRemainingAttemptsDialog;
 
-    private class StateMachine {
+    protected class StateMachine {
         final int ENTER_PUK = 0;
         final int ENTER_PIN = 1;
         final int CONFIRM_PIN = 2;
         final int DONE = 3;
-        private int state = ENTER_PUK;
+        protected int state = ENTER_PUK;
 
         public void next() {
             int msg = 0;
@@ -241,7 +243,7 @@ public class KeyguardSimPukView extends KeyguardAbsKeyInputView
         }
     }
 
-    private Dialog getSimUnlockProgressDialog() {
+    protected Dialog getSimUnlockProgressDialog() {
         if (mSimUnlockProgressDialog == null) {
             mSimUnlockProgressDialog = new ProgressDialog(mContext);
             mSimUnlockProgressDialog.setMessage(
@@ -272,7 +274,7 @@ public class KeyguardSimPukView extends KeyguardAbsKeyInputView
         return mRemainingAttemptsDialog;
     }
 
-    private boolean checkPuk() {
+    protected boolean checkPuk() {
         // make sure the puk is at least 8 digits long.
         if (mPasswordEntry.getText().length() >= 8) {
             mPukText = mPasswordEntry.getText().toString();
@@ -281,7 +283,7 @@ public class KeyguardSimPukView extends KeyguardAbsKeyInputView
         return false;
     }
 
-    private boolean checkPin() {
+    protected boolean checkPin() {
         // make sure the PIN is between 4 and 8 digits
         int length = mPasswordEntry.getText().length();
         if (length >= 4 && length <= 8) {
@@ -295,7 +297,7 @@ public class KeyguardSimPukView extends KeyguardAbsKeyInputView
         return mPinText.equals(mPasswordEntry.getText().toString());
     }
 
-    private void updateSim() {
+    protected void updateSim() {
         getSimUnlockProgressDialog().show();
 
         if (mCheckSimPukThread == null) {
