0dae2cc951ba470b4e7a82afe3f96d663cf33f8b
Lars Greiss <kufikugel@googlemail.com>
2013-11-08 22:59:42 +0100

    Framework: safe headset volume option (1 of 2)
    thanks DvTonder
    
    Change-Id: I5355820a24d48c1d8f54ea17a7022d891ad07857

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2806,6 +2806,12 @@ public final class Settings {
        public static final String STATUS_BAR_BRIGHTNESS_CONTROL = "status_bar_brightness_control";
 
         /**
+         * Whether to prevent loud volume levels when headset is first plugged in.
+         * @hide
+         */
+        public static final String SAFE_HEADSET_VOLUME = "safe_headset_volume";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
