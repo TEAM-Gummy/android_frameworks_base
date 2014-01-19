3fd76cffa2fdd6e110294a385d39b09e16c01acc
maxwen <max.weninger@gmail.com>
2014-01-19 23:06:31 +0100

    Frameworks: Wakelock Blocker (1/2)
    Change-Id: I38114529e535a3168ddb2135b10d7811347d7cf2
    Signed-off-by: Arnav Gosain <nolinuxnoparty@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3828,6 +3828,18 @@ public final class Settings {
         public static final String STATUS_BAR_IME_SWITCHER = "status_bar_ime_switcher";
 
         /**
+         * Enable blocking wakelock
+         * @hide
+         */
+        public static final String WAKELOCK_BLOCKING_ENABLED = "wakelock_blocking_enabled";
+
+        /**
+         * List of wakelock blocks selected
+         * @hide
+         */
+        public static final String WAKELOCK_BLOCKING_LIST = "wakelock_blocking_list";
+
+        /**
          * Automatic keyboard rotation timeout.  0 to disable completely.
          * @hide
          */
