ff2b0ba51b74212fc31929a56682acf921848260
cphelps76 <cphelps76@gmail.com>
2014-01-22 00:15:57 -0600

    Frameworks: port TRDS credit SlimRoms
      - an all inclusive patch for trds minus the theme engine since we support cm theme chooser
    
    Change-Id: I01ca0a4affac15c1ac6547dd40c07328ed33ab01

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5430,6 +5430,13 @@ public final class Settings {
         public static final String UI_NIGHT_MODE = "ui_night_mode";
 
         /**
+         * Whether user activated inverted UI mode or default UI mode. Owned
+         * and controlled by UiModeManagerService.
+         * @hide
+         */
+        public static final String UI_INVERTED_MODE = "ui_inverted_mode";
+
+        /**
          * Whether screensavers are enabled.
          * @hide
          */
@@ -5558,7 +5565,8 @@ public final class Settings {
             MOUNT_UMS_AUTOSTART,
             MOUNT_UMS_PROMPT,
             MOUNT_UMS_NOTIFY_ENABLED,
-            UI_NIGHT_MODE
+            UI_NIGHT_MODE,
+            UI_INVERTED_MODE,
         };
 
         /**
