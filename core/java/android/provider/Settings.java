edb62a0bcb67a30cbe8bede5d799d80e3cecb3de
cphelps76 <cphelps76@gmail.com>
2014-01-24 14:47:12 -0600

    increasing ring feature
    Change-Id: I3bc2c26bc55f2c18bb2924eba06850e0126a2ab6

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2151,6 +2151,24 @@ public final class Settings {
         public static final String VOLUME_LINK_NOTIFICATION = "volume_link_notification";
 
         /**
+         * Whether the phone ringtone should be played in an increasing manner
+         * @hide
+         */
+        public static final String INCREASING_RING = "increasing_ring";
+
+        /**
+         * Minimum volume index for increasing ring volume
+         * @hide
+         */
+        public static final String INCREASING_RING_MIN_VOLUME = "increasing_ring_min_vol";
+
+        /**
+         * Time (in ms) between ringtone volume increases
+         * @hide
+         */
+        public static final String INCREASING_RING_INTERVAL = "increasing_ring_interval";
+
+        /**
          * Whether silent mode should allow vibration feedback. This is used
          * internally in AudioService and the Sound settings activity to
          * coordinate decoupling of vibrate and silent modes. This setting
