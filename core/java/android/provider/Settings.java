8ceef8e9d6a6f48b4689d93fdc270e1d97f0f766
chris phelps <cphelps76@gmail.com>
2014-02-08 20:02:19 -0600

    Revert "[1/3] base: allow disabling non intrusive incall ui"
    This reverts commit a9bf3240220eaa12dd60eaac3450a10ddb15fb40.
    
    Change-Id: I170982febc92f66122abf11d87b2161b4c40d447

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3566,13 +3566,6 @@ public final class Settings {
         public static final String LOCKSCREEN_VIBRATE_ENABLED = "lockscreen.vibrate_enabled";
 
         /**
-         * Should the non-intrsive incall ui be used
-         *
-         * @hide
-         */
-        public static final String NON_INTRUSIVE_INCALL = "non_intrusive_incall";
-
-        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
