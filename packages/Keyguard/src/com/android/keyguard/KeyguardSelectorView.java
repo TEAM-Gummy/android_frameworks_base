30d3e5549d5e8a1261eed11d019ce20ce578de5a
Jubakuba <Jubakuba@gmail.com>
2014-03-12 08:16:38 -0600

    Frameworks: More Lockscreen Colors (1/2)
    No content resolver?  Colors EVERYWHERE!!!
    
    PS2: Code clean-up && fix avatar
    	text color && set avatar
    	border color (AOSP was never
    	using their own color value
    	either <_< so fix that as well
    
    Change-Id: Ie4daaf1588cd2bbff8871c2beea862f2a15faef7

--- packages/Keyguard/src/com/android/keyguard/KeyguardSelectorView.java
+++ packages/Keyguard/src/com/android/keyguard/KeyguardSelectorView.java
@@ -55,6 +55,7 @@ import com.android.internal.telephony.IccCardConstants.State;
 import com.android.internal.util.gummy.AppHelper;
 import com.android.internal.util.gummy.LockscreenTargetUtils;
 import com.android.internal.util.gummy.DeviceUtils;
+import com.android.internal.util.gummy.ImageHelper;
 import com.android.internal.util.gummy.TGActions;
 import com.android.internal.util.gummy.TorchConstants;
 import com.android.internal.view.RotationPolicy;
@@ -285,6 +286,11 @@ public class KeyguardSelectorView extends LinearLayout implements KeyguardSecuri
                 Settings.Secure.LOCKSCREEN_DOTS_COLOR, -2,
                 UserHandle.USER_CURRENT);
 
+        int ringColor = Settings.Secure.getIntForUser(
+                mContext.getContentResolver(),
+                Settings.Secure.LOCKSCREEN_MISC_COLOR, -2,
+                UserHandle.USER_CURRENT);
+
         String lockIcon = Settings.Secure.getStringForUser(
                 mContext.getContentResolver(),
                 Settings.Secure.LOCKSCREEN_LOCK_ICON,
@@ -311,7 +317,7 @@ public class KeyguardSelectorView extends LinearLayout implements KeyguardSecuri
             }
         }
 
-        mGlowPadView.setColoredIcons(lockColor, dotColor, lock);
+        mGlowPadView.setColoredIcons(lockColor, dotColor, ringColor, lock);
 
         updateTargets();
 
@@ -437,9 +443,37 @@ public class KeyguardSelectorView extends LinearLayout implements KeyguardSecuri
                     R.drawable.ic_lockscreen_target_activated);
             final InsetDrawable activeBack = new InsetDrawable(blankActiveDrawable, 0, 0, 0, 0);
 
+            int frontColor = Settings.Secure.getIntForUser(
+                    mContext.getContentResolver(),
+                    Settings.Secure.LOCKSCREEN_TARGETS_COLOR, -2,
+                    UserHandle.USER_CURRENT);
+
+            int backColor = Settings.Secure.getIntForUser(
+                    mContext.getContentResolver(),
+                    Settings.Secure.LOCKSCREEN_MISC_COLOR, -2,
+                    UserHandle.USER_CURRENT);
+
+            Drawable unlockFront = res.getDrawable(R.drawable.ic_lockscreen_unlock_normal);
+            Drawable unlockBack = res.getDrawable(R.drawable.ic_lockscreen_unlock_activated);;
+
+            if (frontColor != -2) {
+                unlockFront = new BitmapDrawable(
+                        res, ImageHelper.getColoredBitmap(unlockFront, frontColor));
+            }
+
+            if (backColor != -2) {
+                unlockBack = new BitmapDrawable(
+                        res, ImageHelper.getColoredBitmap(unlockBack, backColor));
+            }
+
+            int insetType = LockscreenTargetUtils.getInsetForIconType(
+                    mContext, GlowPadView.ICON_RESOURCE);
+            Drawable unlock = LockscreenTargetUtils.getLayeredDrawable(mContext,
+                    unlockBack, unlockFront, insetType, true);
+            TargetDrawable unlockTarget = new TargetDrawable(res, unlock);
+
             // Add unlock target
-            storedDrawables.add(new TargetDrawable(res,
-                    res.getDrawable(R.drawable.ic_lockscreen_unlock)));
+            storedDrawables.add(unlockTarget);
             description.add(getResources().getString(
                 com.android.internal.R.string.description_target_unlock));
             directionDescription.add(getResources().getString(
@@ -490,6 +524,22 @@ public class KeyguardSelectorView extends LinearLayout implements KeyguardSecuri
                     }
 
                     int inset = LockscreenTargetUtils.getInsetForIconType(mContext, type);
+
+                    if (frontColor != -2) {
+                        front = new BitmapDrawable(
+                                res, ImageHelper.getColoredBitmap(front, frontColor));
+                    }
+
+                    if (backColor != -2) {
+                        if ((back instanceof InsetDrawable)) {
+                            back = new BitmapDrawable(res, ImageHelper.getColoredBitmap(
+                                    blankActiveDrawable, backColor));
+                        } else {
+                            back = new BitmapDrawable(res, ImageHelper.getColoredBitmap(
+                                    back, backColor));
+                        }
+                    }
+
                     Drawable drawable = LockscreenTargetUtils.getLayeredDrawable(mContext,
                             back,front, inset, frontBlank);
                     TargetDrawable targetDrawable = new TargetDrawable(res, drawable);
