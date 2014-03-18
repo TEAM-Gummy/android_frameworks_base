b503ea853edd1426fbd28b5eda84ec5d73cee7a0
Lars Greiss <kufikugel@googlemail.com>
2014-03-18 16:33:01 +0100

    Frameworks: Fix linked volume does not persist after reboot.
    The controling boolean value must be set before updateStreamVolumeAlias
    is called first time. Otherwise updateStreamVolumeAlias will assume
    it is always false till user toggle it in settings again.
    
    Change-Id: I53455249909fa6ad69fa0ea6930f9065e5a14fdf

--- media/java/android/media/AudioService.java
+++ media/java/android/media/AudioService.java
@@ -519,6 +519,9 @@ public class AudioService extends IAudioService.Stub {
         mUseFixedVolume = mContext.getResources().getBoolean(
                 com.android.internal.R.bool.config_useFixedVolume);
 
+        mLinkNotificationWithVolume = Settings.System.getIntForUser(mContentResolver,
+                Settings.System.VOLUME_LINK_NOTIFICATION, 1, UserHandle.USER_CURRENT) == 1;
+
         // must be called before readPersistedSettings() which needs a valid mStreamVolumeAlias[]
         // array initialized by updateStreamVolumeAlias()
         updateStreamVolumeAlias(false /*updateVolumes*/);
@@ -800,9 +803,6 @@ public class AudioService extends IAudioService.Stub {
             readDockAudioSettings(cr);
             mSafeVolumeEnabled = new Boolean(safeVolumeEnabled(cr));
         }
-
-        mLinkNotificationWithVolume = Settings.System.getIntForUser(cr,
-                Settings.System.VOLUME_LINK_NOTIFICATION, 1, UserHandle.USER_CURRENT) == 1;
 
         mMuteAffectedStreams = System.getIntForUser(cr,
                 System.MUTE_STREAMS_AFFECTED,
