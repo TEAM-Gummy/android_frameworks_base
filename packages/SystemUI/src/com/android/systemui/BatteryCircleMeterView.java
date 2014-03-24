b3be68fe880780176a5b77cdfe62a5872675fb21
gwindlord <gwindlord@gmail.com>
2014-03-24 20:02:37 +0100

    [1/2] frameworks: Adding old-school (JB like) battery percents style due to its better visibility.
    Also added "No battery" style.
    
    Added percents visibility while charging and
    colorizing percents while charging
    
    One selector made obvious
    
    Removing unnecessary code
    
    Setting green color for text 90+% charged
    
    Now bolt color means color of circle battery, not percents
    
    Fixing percentage color while charging for "text only" style
    
    Fixing percentage color for JB style
    
    Adding green percents color when charging and >90% for circles with percents too
    
    Including Juba's changes regarding battery tile with no battery in statusbar
    and styles order
    
    Line to long and use static reference
    
    How does it look like:
    https://dl.dropboxusercontent.com/u/66673431/BattModPercentScreen1.jpg
    https://dl.dropboxusercontent.com/u/66673431/BattModPercentScreen2.jpg
    
    Settings change: https://gerrit.slimroms.net/#/c/5755/
    
    Change-Id: I7505c7e35c252a0fac0bd37db4be33bbff89ef41

--- packages/SystemUI/src/com/android/systemui/BatteryCircleMeterView.java
+++ packages/SystemUI/src/com/android/systemui/BatteryCircleMeterView.java
@@ -174,7 +174,7 @@ public class BatteryCircleMeterView extends ImageView {
 
         mPathEffect = new DashPathEffect(new float[]{3,2},0);
 
-        updateSettings();
+        updateSettings(false);
     }
 
     @Override
@@ -233,6 +233,14 @@ public class BatteryCircleMeterView extends ImageView {
             padLevel = 100;
         }
 
+        if (mLevel > 14) {
+          if (mIsCharging) {
+            usePaint.setColor(mCircleTextChargingColor);
+          } else {
+            usePaint.setColor(mCircleColor);
+          }
+        }
+
         // draw thin gray ring first
         canvas.drawArc(drawRect, 270, 360, false, mPaintGray);
         // draw colored arc representing charge level
@@ -242,8 +250,8 @@ public class BatteryCircleMeterView extends ImageView {
         if (level < 100 && mPercentage) {
             if (level <= 14) {
                 mPaintFont.setColor(mPaintRed.getColor());
-            } else if (mIsCharging) {
-                mPaintFont.setColor(mCircleTextChargingColor);
+            } else if (mIsCharging && (level > 89)) {
+                mPaintFont.setColor(Color.GREEN);
             } else {
                 mPaintFont.setColor(mCircleTextColor);
             }
@@ -269,7 +277,7 @@ public class BatteryCircleMeterView extends ImageView {
         }
     }
 
-    public void updateSettings() {
+    public void updateSettings(final boolean isQuickSettingsTile) {
         Resources res = getResources();
         ContentResolver resolver = getContext().getContentResolver();
 
@@ -305,10 +313,15 @@ public class BatteryCircleMeterView extends ImageView {
         mRectLeft = null;
         mCircleSize = 0;
 
-        mActivated = (mBatteryStyle == BatteryMeterView.BATTERY_STYLE_CIRCLE ||
-                      mBatteryStyle == BatteryMeterView.BATTERY_STYLE_CIRCLE_PERCENT ||
-                      mBatteryStyle == BatteryMeterView.BATTERY_STYLE_DOTTED_CIRCLE ||
-                      mBatteryStyle == BatteryMeterView.BATTERY_STYLE_DOTTED_CIRCLE_PERCENT);
+        if (isQuickSettingsTile && mBatteryStyle == BatteryMeterView.BATTERY_STYLE_GONE) {
+           mActivated = false;
+        } else {
+           mActivated = (mBatteryStyle == BatteryMeterView.BATTERY_STYLE_CIRCLE ||
+                         mBatteryStyle == BatteryMeterView.BATTERY_STYLE_CIRCLE_PERCENT ||
+                         mBatteryStyle == BatteryMeterView.BATTERY_STYLE_DOTTED_CIRCLE ||
+                         mBatteryStyle == BatteryMeterView.BATTERY_STYLE_DOTTED_CIRCLE_PERCENT);
+        }
+
         mPercentage = (mBatteryStyle == BatteryMeterView.BATTERY_STYLE_CIRCLE_PERCENT ||
                        mBatteryStyle == BatteryMeterView.BATTERY_STYLE_DOTTED_CIRCLE_PERCENT);
 
