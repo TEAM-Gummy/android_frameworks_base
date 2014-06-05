9add37aea9344137f164d1cfb7cd016a07943988
Clark Scheff <clark@cyngn.com>
2014-06-04 18:28:36 -0700

    CM11 Themes: Add support for composed icons [1/2]
    If an icon pack includes background, mask, and/or foreground
    images and an icon is not included for a given app, an icon
    will be generated using the background, mask and foreground.
    
    Icon packs can include the following items into their
    appfilter.xml
        <!-- Set deafult scale factor for the icons -->
        <scale factor="1.0" />
    
        <!-- Set back image for composed icons -->
        <iconback img="iconback" />
    
        <!-- Set the mask image for composed icons
             Original icon will be masked off using this mask -->
        <iconmask img="iconmask" />
    
        <!-- Set foreground image for composed icons -->
        <iconupon img="iconupon" />
    
    Note: Mutliple iconback images can be used as follows
        <iconback img1="iconback1" img2="iconback2" img3="iconback3" />
    
    Change-Id: I0f5fd7de4f0eae485139cbcd282c1d1e1663952f

--- core/java/android/content/res/Resources.java
+++ core/java/android/content/res/Resources.java
@@ -16,11 +16,13 @@
 
 package android.content.res;
 
+import android.app.ComposedIconInfo;
 import com.android.internal.util.XmlUtils;
 
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserException;
 
+import android.app.IconPackHelper.IconCustomizer;
 import android.content.pm.ActivityInfo;
 import android.content.pm.PackageItemInfo;
 import android.graphics.Movie;
@@ -139,6 +141,7 @@ public class Resources {
     private WeakReference<IBinder> mToken;
 
     private SparseArray<PackageItemInfo> mIcons;
+    private ComposedIconInfo mComposedIconInfo;
 
     static {
         sPreloadedDrawables = new LongSparseArray[2];
@@ -705,6 +708,10 @@ public class Resources {
      * @return Drawable An object that can be used to draw this resource.
      */
     public Drawable getDrawable(int id) throws NotFoundException {
+        return getDrawable(id, true);
+    }
+    /** @hide */
+    public Drawable getDrawable(int id, boolean supportComposedIcons) throws NotFoundException {
         //Check if an icon is themed
         PackageItemInfo info = mIcons != null ? mIcons.get(id) : null;
         if (info != null && info.themedIcon != 0) {
@@ -719,7 +726,7 @@ public class Resources {
             } else {
                 mTmpValue = null;
             }
-            getValue(id, value, true);
+            getValue(id, value, true, supportComposedIcons);
         }
         Drawable res = loadDrawable(value, id);
         synchronized (mAccessLock) {
@@ -750,6 +757,11 @@ public class Resources {
      * @return Drawable An object that can be used to draw this resource.
      */
     public Drawable getDrawableForDensity(int id, int density) throws NotFoundException {
+        return getDrawableForDensity(id, density, true);
+    }
+    /** @hide */
+    public Drawable getDrawableForDensity(int id, int density, boolean supportComposedIcons)
+            throws NotFoundException {
         //Check if an icon was themed
         PackageItemInfo info = mIcons != null ? mIcons.get(id) : null;
         if (info != null && info.themedIcon != 0) {
@@ -764,7 +776,7 @@ public class Resources {
             } else {
                 mTmpValue = null;
             }
-            getValueForDensity(id, density, value, true);
+            getValueForDensity(id, density, value, true, supportComposedIcons);
 
             /*
              * Pretend the requested density is actually the display density. If
@@ -1146,6 +1158,11 @@ public class Resources {
      */
     public void getValue(int id, TypedValue outValue, boolean resolveRefs)
             throws NotFoundException {
+        getValue(id, outValue, resolveRefs, true);
+    }
+    /** @hide */
+    public void getValue(int id, TypedValue outValue, boolean resolveRefs,
+                         boolean supportComposedIcons) throws NotFoundException {
         //Check if an icon was themed
         PackageItemInfo info = mIcons != null ? mIcons.get(id) : null;
         if (info != null && info.themedIcon != 0) {
@@ -1153,6 +1170,11 @@ public class Resources {
         }
         boolean found = mAssets.getResourceValue(id, 0, outValue, resolveRefs);
         if (found) {
+            if (supportComposedIcons && mComposedIconInfo != null && info != null &&
+                    info.themedIcon == 0) {
+                Drawable dr = loadDrawable(outValue, id);
+                IconCustomizer.getValue(this, id, outValue, dr);
+            }
             return;
         }
         throw new NotFoundException("Resource ID #0x"
@@ -1174,6 +1196,11 @@ public class Resources {
      */
     public void getValueForDensity(int id, int density, TypedValue outValue, boolean resolveRefs)
             throws NotFoundException {
+        getValueForDensity(id, density, outValue, resolveRefs, true);
+    }
+    /** @hide */
+    public void getValueForDensity(int id, int density, TypedValue outValue, boolean resolveRefs,
+                                   boolean supportComposedIcons) throws NotFoundException {
         //Check if an icon was themed
         PackageItemInfo info = mIcons != null ? mIcons.get(id) : null;
         if (info != null && info.themedIcon != 0) {
@@ -1181,6 +1208,11 @@ public class Resources {
         }
         boolean found = mAssets.getResourceValue(id, density, outValue, resolveRefs);
         if (found) {
+            if (supportComposedIcons && mComposedIconInfo != null && info != null &&
+                    info.themedIcon == 0) {
+                Drawable dr = loadDrawable(outValue, id);
+                IconCustomizer.getValue(this, id, outValue, dr);
+            }
             return;
         }
         throw new NotFoundException("Resource ID #0x" + Integer.toHexString(id));
@@ -2472,6 +2504,16 @@ public class Resources {
     /** @hide */
     public void setIconResources(SparseArray<PackageItemInfo> icons) {
         mIcons = icons;
+    }
+
+    /** @hide */
+    public void setComposedIconInfo(ComposedIconInfo iconInfo) {
+        mComposedIconInfo = iconInfo;
+    }
+
+    /** @hide */
+    public ComposedIconInfo getComposedIconInfo() {
+        return mComposedIconInfo;
     }
 
     private Resources() {
