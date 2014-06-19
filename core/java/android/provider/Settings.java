e89273f7978178ffa5284a0cbd694bdb8c4cdb4d
cphelps76 <cphelps76@gmail.com>
2014-06-19 15:06:11 -0500

    Frameworks: port Peek from PA (1/2)
    Sensors are fully disabled when screen is on. When you turn the screen off,
    it will start polling both gyroscope and proximity sensor values. If it detects
    using the gyroscope sensor that device is lying on a table, it disables proximity
    polling. If it detects with proximity sensor that it is in your pocket, it disables
    gyroscope polling. This way, we save some battery from the beginning. Later, when you
    receive a notification, it enables a partial wakelock (CPU on but at low frequency),
    and starts listening for movement, either from getting it out your pocket or lifting
    off from the table. If it detects those events, it will turn on screen and show the notification.
    Wakelock timeouts on 10 secs, so if you receive a notification and there is no movement in that time,
    sensors will stop polling after that time, disabling peek interaction until a new notification comes.
    Peek doesn't process low and min priority notifications.
    
    https://plus.google.com/+ParanoidAndroidCorner/posts/3wppxakBSVR
    
    Change-Id: I7f0cb08e973e70f7376749ca937f33624fa94168

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4070,6 +4070,14 @@ public final class Settings {
         public static final String DOUBLE_TAP_SLEEP_GESTURE = "double_tap_sleep_gesture";
 
         /**
+         * Either if notification peek is enabled of disabled
+         * 0 = disabled (default)
+         * 1 = enabled
+         * @hide
+         */
+        public static final String PEEK_STATE = "peek_state";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
